096281c8687c57ffad70cc01999e2a822da50d07
ball0514 <ball070428@gmail.com>
2024-01-12 15:41:34 +0800

    v2  dots of loading

--- js/busMarker.js
+++ js/busMarker.js
@@ -1,31 +1,3 @@
-const dot = document.querySelectorAll(".dot");
-function loading() {
-  dot.forEach((dot, index) => {
-    switch (index) {
-      case 0:
-        setTimeout(() => {
-          dot.classList.remove("hidden");
-        }, 500);
-        break;
-      case 1:
-        setTimeout(() => {
-          dot.classList.remove("hidden");
-        }, 1000);
-        break;
-      case 2:
-        setTimeout(() => {
-          dot.classList.remove("hidden");
-        }, 1500);
-        break;
-    }
-  });
-  dot.forEach((dot) => {
-    dot.classList.add("hidden");
-  });
-}
-loading();
-const repeat = setInterval(loading, 2000);
-
 const swap = document.querySelector("#swap");
 const start = document.querySelector("#start");
 const end = document.querySelector("#end");
--- js/collection.js
+++ js/collection.js
@@ -1,31 +1,3 @@
-const dot = document.querySelectorAll(".dot");
-function loading() {
-  dot.forEach((dot, index) => {
-    switch (index) {
-      case 0:
-        setTimeout(() => {
-          dot.classList.remove("hidden");
-        }, 500);
-        break;
-      case 1:
-        setTimeout(() => {
-          dot.classList.remove("hidden");
-        }, 1000);
-        break;
-      case 2:
-        setTimeout(() => {
-          dot.classList.remove("hidden");
-        }, 1500);
-        break;
-    }
-  });
-  dot.forEach((dot) => {
-    dot.classList.add("hidden");
-  });
-}
-loading();
-const repeat = setInterval(loading, 2000);
-
 const stopName = document.querySelector("#stop");
 const busName = document.querySelector("#bus");
 const noList = document.querySelector(".noList");
